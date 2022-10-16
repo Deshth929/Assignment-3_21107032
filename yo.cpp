7a2063b58f4398677672c53d60ec9a756317236e
Deshth929 <105069840+Deshth929@users.noreply.github.com>
2022-10-16 23:49:29 +0530

    Delete yo.cpp

--- yo.cpp
+++
@@ -1,9 +0,0 @@
-// Assignment 3 of dsa
-int binary_search(int arr[],int d,int l,int k){
-    int m=(d+l)/2; 
-    while(d<=l){   
-    if(arr[d]==k){return m;}
-    if(arr[d]==k){return m;}  
-    else{
-        if(arr[m]<k){d=m+1;}
-        else{l=m-1;}
